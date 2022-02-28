429f8fe0874088a8689c7029d57093d0276eab1c
Doc077 <dom.rajkowski@gmail.com>
2022-02-28 20:07:38 +0100

    Initial commit

---
+++ src/app/app.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Request, Response, ViewResponse } from '@melonly/core'
+
+@Controller()
+export class AppController {
+    constructor(private readonly request: Request, private readonly response: Response) {}
+
+    @Get('/')
+    public index(): ViewResponse {
+        return this.response.render('home', {
+            message: 'Hello World',
+        })
+    }
+}
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { App } from '@melonly/core'
+
+import { AppController } from './app/app.controller'
+
+App.createApp([
+    AppController,
+])
