fbf472c1fc7c888d39ffee4579149f6acd60774f
Doc077 <dom.rajkowski@gmail.com>
2022-03-17 20:39:17 +0100

    Update default migration

--- database/migrations/create-users-table.ts
+++ database/migrations/create-users-table.ts
@@ -1,9 +1,11 @@
-export async function up(schema: any): Promise<void> {
+import { Schema } from '@melonly/core'
+
+export async function up(schema: Schema): Promise<void> {
     schema.createTable('users', {
         id: 'id',
         name: 'string',
         email: 'string',
         password: 'string',
-        created_at: 'date',
+        createdAt: 'datetime',
     })
 }
