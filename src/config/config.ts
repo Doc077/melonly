fac7e221ca44dab27f517335823f76134e0f436f
Doc077 <dom.rajkowski@gmail.com>
2022-04-02 18:35:25 +0200

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,19 +1,23 @@
+const { env } = process
+
 export default {
   app: {
     name: 'Melonly',
     language: 'en',
+    debug: env.APP_DEBUG,
+    key: env.APP_KEY,
   },
   database: {
-    connection: process.env.DB_CONNECTION,
-    host: process.env.DB_HOST,
-    name: process.env.DB_DATABASE,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    connection: env.DB_CONNECTION,
+    host: env.DB_HOST,
+    name: env.DB_DATABASE,
+    username: env.DB_USERNAME,
+    password: env.DB_PASSWORD,
   },
   mail: {
-    service: process.env.MAIL_SERVICE,
-    address: process.env.MAIL_ADDRESS,
-    host: process.env.MAIL_HOST,
-    password: process.env.MAIL_PASSWORD,
+    service: env.MAIL_SERVICE,
+    address: env.MAIL_ADDRESS,
+    host: env.MAIL_HOST,
+    password: env.MAIL_PASSWORD,
   },
 }
