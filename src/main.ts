a993c0c54369abaa42daf43ee77980e89f28c023
Doc077 <dom.rajkowski@gmail.com>
2022-04-02 17:27:49 +0200

    Add config.ts & improve directory structure

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from '@melonly/core'
-import { AppController } from './app/app.controller'
+import { AppController } from './modules/app/app.controller'
 
 const app = createApp()
 
