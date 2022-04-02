72d20c1483458e57d11c9456cde6a5c9c4192b6c
Doc077 <dom.rajkowski@gmail.com>
2022-04-02 17:40:23 +0200

    Add config.ts options

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,19 @@
 export default {
-  appName: '',
-  language: 'en',
+  app: {
+    name: 'Melonly',
+    language: 'en',
+  },
+  database: {
+    connection: process.env.DB_CONNECTION,
+    host: process.env.DB_HOST,
+    name: process.env.DB_DATABASE,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+  },
+  mail: {
+    service: process.env.MAIL_SERVICE,
+    address: process.env.MAIL_ADDRESS,
+    host: process.env.MAIL_HOST,
+    password: process.env.MAIL_PASSWORD,
+  },
 }
