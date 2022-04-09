7acadacedf15388f81b4570e58e7534b29090499
Doc077 <dom.rajkowski@gmail.com>
2022-04-09 20:37:50 +0200

    Add default user model

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, RenderResponse, Request, Response } from '@melonly/core'
+import { User } from './users/user.model'
 
 @Controller()
 export class AppController {
@@ -6,6 +7,8 @@ export class AppController {
 
   @Get('/')
   public index(): RenderResponse {
+    const users = User.select().where('name', 'like', `%name%`).where('email', '>', 'a').orWhere('age', '=', 20).fetch()
+
     return this.response.render('home', {
       message: 'Hello World',
     })
---
+++ src/app/users/user.model.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, Model } from '@melonly/core'
+
+@Model()
+export class User extends Entity {
+  @Column()
+  public id: number
+
+  @Column()
+  public name: string
+
+  @Column()
+  public email: string
+}
