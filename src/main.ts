289923f843bccd7de60a73e81f75cd6d95dc8664
Doc077 <dom.rajkowski@gmail.com>
2022-05-11 18:27:10 +0200

    Update directory structure

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createAppServer } from '@melonly/core'
-import { AppController } from './app/app.controller'
+import { AppController } from './controllers/app.controller'
 
 const server = createAppServer()
 
